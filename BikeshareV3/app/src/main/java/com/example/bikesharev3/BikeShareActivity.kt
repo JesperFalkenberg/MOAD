a27bd400c5a6404fafc69250b06f3ad31af5562c
JesperFalkenberg <jesperfalkenberg@outlook.dk>
2020-02-19 15:59:38 +0100

    classes and xml filled out, still buggy does not start

---
+++ BikeshareV3/app/src/main/java/com/example/bikesharev3/BikeShareActivity.kt
@@ -0,0 +1,38 @@
+package com.example.bikesharev3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class BikeShareActivity : AppCompatActivity() {
+
+    private lateinit var sButton: Button
+    private lateinit var  eButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bike_share)
+
+        // FINDING BUTTONS
+        sButton = findViewById(R.id.start_ride_button)
+        eButton = findViewById(R.id.end_ride_button)
+
+        // START_BUTTON CHANGE PAGE
+        sButton.setOnClickListener(View.OnClickListener {
+            // START START_RIDE_ACTIVITY
+            val intent = Intent(this, StartRideActivity::class.java)
+            startActivity(intent)
+        })
+
+        // END_BUTTON CHANGE PAGE
+        eButton.setOnClickListener(View.OnClickListener {
+            // START END_RIDE_ACTIVITY
+            val intent = Intent(this, EndRideActivity::class.java)
+            startActivity(intent)
+        })
+    }
+
+
+}
